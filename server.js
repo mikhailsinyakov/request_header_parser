c82a34add8005c95f24927e1e992f6db822da3c0
Glitch (sharp-taxicab) <none>
2017-12-10 10:57:25 +0000

    😇🛫 Checkpoint server.js:586560

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.route("/")
-    .get((req, res) => {
+app.get("/", (req, res) => {
       console.log(req.ip)
       const obj = {
         ipaddress: req.ip,
-        language
+        language: ""
       };
       res.json(obj);
     });
 
-app.listen(port, () => {
-  console.log('Your app is listening on port ' + port);
-});
+app.listen(port, () => console.log('Your app is listening on port ' + port));
